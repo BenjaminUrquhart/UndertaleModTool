7e36b68f87590b1ef13241eba2e5afae23cf544e
Benjamin Urquhart <benjamin@ericsart.com>
2024-02-28 23:27:32 -0800

    move try catch stuff

---
+++ UndertaleModLib/Decompiler/Instructions/Decompiler.TempExceptionValue.cs
@@ -0,0 +1,29 @@
+﻿using static UndertaleModLib.Decompiler.Decompiler;
+
+namespace UndertaleModLib.Decompiler;
+
+public class TempExceptionValue : Expression
+{
+    private Block tryBlock, catchBlock;
+
+    public TempExceptionValue(Block tryBlock, Block catchBlock)
+    {
+        this.catchBlock = catchBlock;
+        this.tryBlock = tryBlock;
+    }
+
+    public override Statement CleanStatement(DecompileContext context, BlockHLStatement block)
+    {
+        return this;
+    }
+
+    public override string ToString(DecompileContext context)
+    {
+        return "// TempExceptionValue " + tryBlock + " " + catchBlock;
+    }
+
+    internal override AssetIDType DoTypePropagation(DecompileContext context, AssetIDType suggestedType)
+    {
+        return suggestedType;
+    }
+}
---
+++ UndertaleModLib/Decompiler/Instructions/Decompiler.TryCatchHLStatement.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace UndertaleModLib.Decompiler;
+
+public static partial class Decompiler
+{
+    public class TryCatchHLStatement : BlockHLStatement
+    {
+        public Block tryBlock, catchBlock;
+
+        public TryCatchHLStatement(Block tryBlock, Block catchBlock = null)
+        {
+            this.catchBlock = catchBlock;
+            this.tryBlock = tryBlock;
+        }
+
+        public new BlockHLStatement CleanBlockStatement(DecompileContext context)
+        {
+            if (tryBlock.Statements != null)
+                Statements = tryBlock.Statements;
+            return base.CleanBlockStatement(context);
+        }
+
+        internal override AssetIDType DoTypePropagation(DecompileContext context, AssetIDType suggestedType)
+        {
+            return suggestedType;
+        }
+
+        public override string ToString(DecompileContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            Statements = tryBlock.Statements;
+            sb.AppendLine("try");
+            sb.Append(context.Indentation);
+            sb.AppendLine(ToString(context, false));
+
+            if (catchBlock != null)
+            {
+                Statements = catchBlock.Statements.Skip(1).ToList();
+                sb.Append(context.Indentation);
+                sb.Append("catch(");
+                sb.Append(catchBlock.Instructions[0].Destination.Target.Name.Content);
+                sb.AppendLine(")");
+                sb.Append(context.Indentation);
+                sb.AppendLine(ToString(context, false));
+                Statements = tryBlock.Statements;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
